1272fc5db6accce3f0297cf8230f5d3265de9930
gabbydgab <gamba@gabbydgab.com>
2019-06-19 11:31:07 +0800

    initial structure

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types = 1);
+
+use Illuminate\Database\Seeder;
+
+final class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() : void
+    {
+        // $this->call(UsersTableSeeder::class);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+This project is using [laravel framework](https://laravel.com/docs/5.7) version 5.7.
